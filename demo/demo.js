9aef9384478bf1bab05b554eca0890d7c877b472
Steven Hauser <stevenhauser@gmail.com>
2013-09-16 20:37:03 -0400

    Continue to beef up demo

--- demo/demo.js
+++ demo/demo.js
@@ -3,15 +3,43 @@
   var Model = Backbone.Model.extend({}),
 
       ProxyModel = Backbone.RivetableProxyModel.extend({
+
+        initialize: function() {
+          this.listenTo(this.model.collection, "destroy", this.onCollectionChange);
+          this.listenTo(this.model.collection, "add", this.onCollectionChange);
+        },
+
         fullName: function() {
           return [this.get("firstName"), this.get("lastName")].join(" ");
+        },
+
+        isFirst: function() {
+          return this.model.collection.first() === this.model;
+        },
+
+        isMan: function() { return this.get("firstName") === "annyong"; },
+
+        isWoman: function() { return this.get("firstName") === "lindsay"; },
+
+        onCollectionChange: function() {
+          this.set("isFirst", this.isFirst());
+          console.log( this.toJSON() );
+        }
+
+      });
+
+      RivetableView = Backbone.RivetableView.extend({
+        rivetableModel: ProxyModel
+      }),
+
+      RegularView = Backbone.View.extend({
+        rivetableModel: ProxyModel,
+        initialize: function() {
+          this.rivetable();
         }
       });
 
-      View = Backbone.RivetableView.extend({
-        rivetableModel: ProxyModel
-      });
-
+  _.extend(RegularView.prototype, Backbone.rivetable);
 
   window.model = new Model({
     firstName: "Annyong",
@@ -20,12 +48,24 @@
     description: "saboteur"
   });
 
-  window.view = new View({
+  window.collection = new Backbone.Collection([window.model, {}])
+
+  window.view = new RivetableView({
     model: window.model,
-    el: "#template"
+    el: document.querySelector("#template").content.cloneNode(true)
   });
+
+  // @TODO: This breaks bindings, unsure why
+  // window.otherView = new RegularView({
+  //   model: window.model,
+  //   el: document.querySelector("#template").content.cloneNode(true)
+  // });
 
   console.log( window.view );
   console.log( window.model );
+
+  var playground = document.querySelector("#playground")
+  playground.appendChild(window.view.el);
+  // playground.appendChild(window.otherView.el);
 
 }());
