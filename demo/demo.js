5133e32558190592b8059b7ccef9fe8fae3ab67b
Steven Hauser <stevenhauser@gmail.com>
2013-09-16 20:10:35 -0400

    Beef up demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,31 @@
 (function() {
 
-  var View = Backbone.RivetableView.extend({});
+  var Model = Backbone.Model.extend({}),
 
-  window.view = new View()
+      ProxyModel = Backbone.RivetableProxyModel.extend({
+        fullName: function() {
+          return [this.get("firstName"), this.get("lastName")].join(" ");
+        }
+      });
+
+      View = Backbone.RivetableView.extend({
+        rivetableModel: ProxyModel
+      });
+
+
+  window.model = new Model({
+    firstName: "Annyong",
+    lastName: "Bluth",
+    title: "adopted child",
+    description: "saboteur"
+  });
+
+  window.view = new View({
+    model: window.model,
+    el: "#template"
+  });
+
   console.log( window.view );
+  console.log( window.model );
 
 }());
