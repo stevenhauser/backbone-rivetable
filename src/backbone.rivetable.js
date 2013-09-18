920bb4bf53a50989ed9ceafc64ad761cfe5c8d3e
Steven Hauser <stevenhauser@gmail.com>
2013-09-18 18:37:58 -0400

    Move rivetable-adapter into rivetable.

--- src/backbone.rivetable.js
+++ src/backbone.rivetable.js
@@ -15,6 +15,39 @@
 
 
 
+  /* Adapter ---------------------------------------- */
+
+  // Lifted right from rivetsjs.com's homepage with a few minor tweaks.
+  // http://rivetsjs.com/#configure
+
+  rivets.configure({
+
+    prefix: "rv",
+
+    adapter: {
+
+      subscribe: function(obj, keypath, callback) {
+        obj.on("change:" + keypath, callback);
+      },
+
+      unsubscribe: function(obj, keypath, callback) {
+        obj.off("change:" + keypath, callback);
+      },
+
+      read: function(obj, keypath) {
+        return obj.get(keypath);
+      },
+
+      publish: function(obj, keypath, value) {
+        obj.set(keypath, value);
+      }
+
+    }
+
+  });
+
+
+
   /* ProxyModel ---------------------------------------- */
 
   var ProxyModel = Backbone.Model.extend({
